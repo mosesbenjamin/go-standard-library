f8281edad36be44446bd36a4bc5bd1c141893b3f
mosesbenjamin <mavewreek@gmail.com>
2022-06-18 19:20:49 -0500

    creating types at runtime

--- reflection/customtype.go
+++ reflection/customtype.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"fmt"
-	"media"
 )
 
 func main() {
 	fmt.Println("My Favorite Movie")
 
-	var myFav media.Catalogable = &media.Movie{}
+	// var myFav media.Catalogable = &media.Movie{}
 
-	myFav.NewMovie("Top Gun", media.PG, 32.8)
+	// myFav.NewMovie("Top Gun", media.PG, 32.8)
 
-	fmt.Printf("My favorite movie is %s\n", myFav.GetTitle())
-	fmt.Printf("It was rated %s\n", myFav.GetRating())
-	fmt.Printf("It made %f in the box office\n", myFav.GetBoxOffice())
+	// fmt.Printf("My favorite movie is %s\n", myFav.GetTitle())
+	// fmt.Printf("It was rated %s\n", myFav.GetRating())
+	// fmt.Printf("It made %f in the box office\n", myFav.GetBoxOffice())
 
-	myFav.SetTitle("Dumb and Dumber")
+	// myFav.SetTitle("Dumb and Dumber")
 
-	fmt.Printf("My favorite movie now is %s\n", myFav.GetTitle())
+	// fmt.Printf("My favorite movie now is %s\n", myFav.GetTitle())
 
 }
